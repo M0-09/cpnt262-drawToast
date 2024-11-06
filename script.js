a51364c757d883311420e985062fa1dc6b6f7325
Moksham <mokshamsharma09@gmail.com>
2024-11-06 10:07:29 -0700

    code outline draft 1

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
+// toggles
+
+let isTImerDOne = false;
+
+let isToast = false;
+
 // bread variable
-
-// isTimerDone
-
-// isBreadInToaster
-
-//isToast
+const bread = {
+  size: [60, 40, 5],
+  burnPoint: 600,
+  toastPoint: 300,
+  currentTemp: 20,
+};
 
 // toaster variable
+const toaster = {
+  slots: 4,
+  button: (e) => {
+    if (e) {
+      return true;
+    }
+    return false;
+  },
+};
 
 /* FUNCTIONS */
+
+//cageDown
+function cageDown(e) {
+  if (e) {
+    // turn on heat
+    activateHeat();
+    // start timer
+    setTimer();
+  }
+}
+
+//cageUp TODO: merge with cageDown function
+
+// timer
+function setTimer(e, time = 120) {
+  if (e) {
+    // run activate heat
+    activateHeat();
+    // start timer at length of time argument
+  }
+  // TODO: doesn't feel right: toggle case && disable heat element
+}
+//activateHeat
+function activateHeat() {
+  // increase bread temp
+  // TODO: as loop increases, apply value to bread current temp consistently
+}
